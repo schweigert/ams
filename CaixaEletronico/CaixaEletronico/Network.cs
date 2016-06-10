b01a9778839c6ffdd8deb946cc586bca6626c03f
fleyhe <fleyhe0@gmail.com>
2016-06-10 16:08:47 -0300

    AMS
    AMS

---
+++ CaixaEletronico/CaixaEletronico/Network.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fleyhe.Network
+{
+    public class Listener
+    {
+
+    }
+
+    public class Bridge
+    {
+
+    }
+
+    public class Event
+    {
+
+    }
+}
