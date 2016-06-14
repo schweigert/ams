1d03f1b6a9ed8c247c87d57b9c67856678585709
Marlon Henry Schweigert <fleyhe0@gmail.com>
2016-06-13 21:51:26 -0300

    AMS
    AMS

--- Servidor/server.rb
+++ Servidor/server.rb
@@ -1,50 +1,17 @@
-require 'SuricatoNetwork' 	#gem install SuricatoNetwork -> 1.0.3
-require 'sqlite3' 			#gem install sqlite3
+# Para instalar a biblioteca do Servidor:
+#      Instalar SQLite3 na máquina
+#      gem install sqlite3
+#      gem install Fleyhe
 
-class Database
-	def self.execute arg
-		return @@db.execute arg
-	end
+require 'Fleyhe'
 
-	def self.open
-		@@db = SQLite3::Database.open "database.db"
-		@@db.results_as_hash = true
-	end
+class MainEvent < Fleyhe::Network::Event; end
 
-	def self.close
-		@@db.close
-	end
-end
-
-class Login < SuricatoNetwork::Event
-	# @form [0] = Nome
-	# @form [1] = Senha
-
+class Log < MainEvent
 	def solve
-
-		a = Database.execute "SELECT senha FROM Clientes WHERE nome = '#{@form[0].to_s.chomp}'"
-		if a[0]['senha'] == @form[1].to_s.chomp
-			@event = "Login"
-
-		else
-			puts "#{@form[0].chomp} recusou o erro"
-			@event = "InvalidLogin"
-		end
-
-	
+		
 	end
 end
 
-# Open Database
-
-Database.open
-
-# Start Network
-SuricatoNetwork::Listener.new 3030
-
-loop {
-
-	puts Time.now
-	sleep 60
-
-}
+$db = Fleyhe::Data::Database.new "database.db"
+$listener = Fleyhe::Network::Database.new 3030, MainEvent
