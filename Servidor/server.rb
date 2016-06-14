ac9d87fd8fc6eb9dda66c4409ddef0e27bd73554
Marlon Henry Schweigert <fleyhe0@gmail.com>
2016-06-13 21:57:05 -0300

    Server
    server

--- Servidor/server.rb
+++ Servidor/server.rb
@@ -3,9 +3,37 @@ require 'Fleyhe' 	#gem install Fleyhe
 class Log < Fleyhe::Network::Event
 
 	def solve
-		@event 
+		
+
+
 	end
 
 end
 
-listener = Fleyhe::Network::Listener.new 3030, Event
+
+
+
+module EDA
+
+	class Funcionario
+
+		def initialize login, password
+
+
+
+		end
+
+	end
+
+end
+
+
+$db = Fleyhe::Data::Database.new "database.db"
+$listener = Fleyhe::Network::Listener.new 3030, Event
+
+loop {
+
+	puts Time.now
+	sleep 60
+
+}
