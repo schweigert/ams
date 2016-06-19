9d923efedf1a0747ba06f8168af8dd58aff5bdc5
Marlon Henry Schweigert <fleyhe0@gmail.com>
2016-06-19 20:46:11 -0300

    Tela Extrato

--- CaixaEletronico/CaixaEletronico/TelaExtrato.cs
+++ CaixaEletronico/CaixaEletronico/TelaExtrato.cs
@@ -15,5 +15,10 @@ namespace CaixaEletronico
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
